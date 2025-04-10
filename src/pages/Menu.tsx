fa806b36b216b5bb94d5f3ca48687f893141f4a2
Adrian Trandafir <adrian_trandafir@epam.com>
2025-04-10 23:13:12 +0300

    create search on sort features for products

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,13 +1,58 @@
 import { useState } from 'react'
-import { menuData } from '../data/menuData'
+import {
+  menuData,
+  Product,
+  MenuCategory,
+  SortDirection
+} from '../data/menuData'
 import ProductCard from 'components/ProductCard'
 import classNames from 'classnames'
+import CartSummary from 'components/CartSummary'
+import totalCartItems from 'utils/calculateTotalProducts'
+import EmptyCart from 'components/EmptyCart'
+import updateProductQuantity from 'utils/updateProductQuantity'
 
 const Menu = () => {
-  const [products, setProducts] = useState([])
+  const [menuProducts, setMenuProducts] = useState<MenuCategory[]>(menuData)
   const [selectedCategories, setSelectedCategories] = useState<Set<string>>(
     new Set()
   )
+  const [searchText, setSearchText] = useState('')
+  const [sortDirection, setSortDirection] = useState<
+    SortDirection | undefined
+  >()
+
+  const productsToDisplay = menuProducts
+    .filter(({ category }) => {
+      return selectedCategories.size === 0 || selectedCategories.has(category)
+    })
+    .flatMap(({ products }) => products)
+    .filter(({ name }) => name.includes(searchText))
+    .sort((a, b) => {
+      if (sortDirection === 'ASC') {
+        return a.price - b.price
+      }
+
+      if (sortDirection === 'DESC') {
+        return b.price - a.price
+      }
+
+      return 0
+    })
+
+  const cartItems = menuProducts.flatMap((category) =>
+    category.products.filter((product) => product.quantity > 0)
+  )
+
+  // const increaseItemQuantity = (id: string, quantity = 1) => {
+  //   setMenuProducts(updateProductQuantity({ id, menuProducts, quantity }))
+  // }
+
+  // const decreaseItemQuantity = (id: string, quantity = 1) => {
+  //   setMenuProducts(
+  //     updateProductQuantity({ id, menuProducts, quantity: -quantity })
+  //   )
+  // }
 
   function updateCategories(
     category: string,
@@ -26,8 +71,48 @@ const Menu = () => {
     })
   }
 
+  function updateAvailability(
+    productId: string,
+    e: React.ChangeEvent<HTMLInputElement>
+  ) {
+    const isChecked = e.target.checked
+
+    setMenuProducts((items) => {
+      return items.map((item) => {
+        return {
+          ...item,
+          products: item.products.map((product) => {
+            if (product.id === productId) {
+              product.available = isChecked
+            }
+
+            return product
+          })
+        }
+      })
+    })
+  }
+
   return (
     <>
+      <div>
+        <input
+          placeholder="Search..."
+          onChange={(e) => setSearchText(e.target.value || '')}
+        />
+      </div>
+
+      <div>
+        <select
+          name="price-sort"
+          onChange={(e) => setSortDirection(e.target.value as SortDirection)}
+        >
+          <option value="ASC">Ascendent</option>
+          <option value="DESC">Descendent</option>
+          <option value=""></option>
+        </select>
+      </div>
+
       <div className="container grid gap-8 lg:grid-cols-[.7fr_.3fr] lg:gap-10">
         <div className="grid gap-12">
           <div className="grid gap-4 justify-items-center">
@@ -58,23 +143,41 @@ const Menu = () => {
           </div>
 
           <ul className="grid max-w-[800px] ss:grid-cols-auto-fit-250 sm:grid-cols-3 gap-8">
-            {menuData
-              .filter(
-                ({ category }) =>
-                  selectedCategories.size === 0 ||
-                  selectedCategories.has(category)
+            {productsToDisplay.map((product) => {
+              return (
+                <li key={product.id}>
+                  <ProductCard {...product} />
+                </li>
               )
-              .map((category) =>
-                category.products.map((product) => {
-                  return (
-                    <li key={product.id}>
-                      <ProductCard product={product} />
-                    </li>
-                  )
-                })
-              )}
+            })}
+          </ul>
+
+          <h2>Availability list</h2>
+          <ul>
+            {menuProducts.map((category) =>
+              category.products.map((product) => {
+                return (
+                  <li key={product.id}>
+                    <label>
+                      <input
+                        type="checkbox"
+                        checked={product.available}
+                        onChange={(e) => updateAvailability(product.id, e)}
+                      />
+                      {product.name}
+                    </label>
+                  </li>
+                )
+              })
+            )}
           </ul>
         </div>
+
+        {totalCartItems(cartItems) > 0 ? (
+          <CartSummary menuProducts={menuProducts} cartItems={cartItems} />
+        ) : (
+          <EmptyCart />
+        )}
       </div>
     </>
   )
