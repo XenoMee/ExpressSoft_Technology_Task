0b3a8b2d2e19e466aa45199ea0dc6302e63018f8
Adrian Trandafir <adrian_trandafir@epam.com>
2025-04-15 12:23:08 +0300

    create card component to display product details to the user

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,25 +1,53 @@
+import ProductQuantityButton from 'common/ProductQuantityButton'
 import { Product } from 'data/menuData'
 
-type ProductCardProps = Product
+type ProductCardProps = Product & {
+  addToCart: () => void
+  removeFromCart: () => void
+}
 
 const ProductCard = (props: ProductCardProps) => {
-  const { name, price, description, available, quantity } = props
+  const {
+    name,
+    price,
+    description,
+    available,
+    addToCart,
+    quantity,
+    removeFromCart
+  } = props
 
   return (
-    <div className="grid gap-10">
-      <div className="grid">
-        <p className="heading3">{name}</p>
-        <p className=" text-rose-500 text-sm">{description}</p>
+    <div className="grid grow">
+      <p className="heading3">{name}</p>
+      <p className=" text-rose-500 text-sm">{description}</p>
+      <div className="flex items-center justify-between">
         <span className="text-primary text-base font-semibold ">
           ${price.toFixed(2)}
         </span>
 
         {available ? (
-          <button className="button btn-primary justify-self-center">
-            Add to Cart
-          </button>
+          <>
+            {quantity > 0 ? (
+              <ProductQuantityButton
+                productQuantity={quantity}
+                increaseQuantity={addToCart}
+                decreaseQuantity={removeFromCart}
+              />
+            ) : (
+              <button
+                className="button btn-primary justify-self-center"
+                onClick={addToCart}
+              >
+                Add to Cart
+              </button>
+            )}
+          </>
         ) : (
-          <button className="button" disabled>
+          <button
+            className="button bg-white text-rose-900 cursor-default"
+            disabled
+          >
             Not available
           </button>
         )}
