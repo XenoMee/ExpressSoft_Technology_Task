ccf599131f025759eb8e64100bd7f3c004dc27ab
Adrian Trandafir <adrian_trandafir@epam.com>
2025-04-15 15:15:11 +0300

    create home page component that redirect to Menu component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,20 @@
-const Home = () => {
-  return <h1>Home Page</h1>;
-};
+import { Link } from 'react-router-dom'
 
-export default Home;
+const Home = () => {
+  return (
+    <main className="grid home-hero">
+      <div className="absolute inset-0 bg-black bg- bg-opacity-60 flex flex-col items-center justify-center px-4 text-center">
+        <div className="grid gap-6">
+          <blockquote className="text-center text-2xl text-white">
+            “You will eat less than you desire and more than you deserve.”
+          </blockquote>
+          <button className="button btn-primary mx-auto">
+            <Link to="/menu">Explore the Menu</Link>
+          </button>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default Home
