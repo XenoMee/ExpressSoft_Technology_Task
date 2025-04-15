118733dda53377c01b82830b4db0bb3dffdc49ea
Adrian Trandafir <adrian_trandafir@epam.com>
2025-04-15 12:22:20 +0300

    create component to display the users selected products for purchase

--- src/components/CartSummary.tsx
+++ src/components/CartSummary.tsx
@@ -1,23 +1,48 @@
-import { MenuCategory, Product } from 'data/menuData'
+import CartItem from 'common/CartProduct'
+import { Product } from 'data/menuData'
+import totalPrice from 'utils/calculateTotalPrice'
 
 type CartSummaryProps = {
-  menuProducts: MenuCategory[]
-  cartItems: Product[]
+  removeFromCart: (productId: string) => void
+  productsInCart: number
+  products: Product[]
+  toggleModal: () => void
 }
 
-const CartSummary = ({ menuProducts, cartItems }: CartSummaryProps) => {
+const CartSummary = ({
+  products,
+  productsInCart,
+  removeFromCart,
+  toggleModal
+}: CartSummaryProps) => {
+  const cartItems = products.filter((product) => product.quantity > 0)
+  const totalProductPrice = totalPrice(cartItems)
+
   return (
     <aside className="bg-white p-5 rounded-md grid gap-5 content-start">
-      <h2 className="heading2 text-primary">Your cart ({})</h2>
+      <h2 className="heading2 text-primary">Your cart ({productsInCart})</h2>
 
-      <ul className="grid gap-4" role="list"></ul>
+      <ul className="grid gap-4">
+        {cartItems.map((product) => (
+          <CartItem
+            key={product.id}
+            name={product.name}
+            amount={product.quantity}
+            price={product.price}
+            removeItem={() => removeFromCart(product.id)}
+          />
+        ))}
+      </ul>
 
       <div className="flex justify-between items-center text-rose-900">
         <p>Order total</p>
-        <p className="font-bold text-2xl">${}</p>
+        <p className="font-bold text-2xl">${totalProductPrice.toFixed(2)}</p>
       </div>
 
-      <button className="button py-4 bg-primary text-white text-base hover:text-white hover:bg-primary-dark">
+      <button
+        className="button py-4 bg-primary text-white text-base hover:text-white hover:bg-primary-dark"
+        onClick={toggleModal}
+      >
         Confirm Order
       </button>
     </aside>
